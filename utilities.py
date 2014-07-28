f22b8a56e3632624b653395a78debd43aa547713
Alex Danoff <aadanoff@gmail.com>
2014-07-28 15:47:25 -0400

    Read in .bookmark file by lines vs splitting
    This commit fixes a bug that prevents the program from working properly
    when the user manually edits the .bookmark file.

--- utilities.py
+++ utilities.py
@@ -179,7 +179,7 @@ def get_last_line(fname):
 
     #get last line
     with open(fname, "r+") as file:
-        all_lines = file.read().split("\n")
+        all_lines = file.readlines()
         last_line = all_lines[-1]        
         del all_lines[-1]
 
@@ -187,14 +187,13 @@ def get_last_line(fname):
     try:
 
         int(last_line)
-        #print("it's a number")      
+
         #write back all the lines except the last one
         with open(fname, "w+") as file:
             for line in all_lines:
                 file.write(line + "\n")    
 
     except ValueError:
-        #print("it's not a number")
         pass
 
     return last_line
